e8a6fe09514691cc45c898b7adb2419d889e224e
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-11-29 21:31:59 +0200

    added form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Form from './Form/Form';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Form/>
+       </> 
+    );
+  }
+}
+
+export default App;
